a7532ab8ec42c7e66ad70a23dfbf8580576b8557
222gremlins <5300207@spsd.sk.ca>
2025-11-21 09:52:38 -0600

    added more to the customer and patty classes

--- sketch.js
+++ sketch.js
@@ -11,19 +11,44 @@ const MAX_PATIENCE = 100;
 
 class Customer {
   constructor() {
+    // can i put generateOrder as this.order...?
     this.order = ;
     this.patience = MAX_PATIENCE;
+
+  }
+
+  generateOrder() {
 
   }
 }
 
 // either make this class an extention or maybe not depending on how it all goes
 class Patty {
-  constructor() {
-    this.state = raw;
-    this.dragging = false;
+  constructor(x, y) {
+    // setting size/location
+    this.x = x;
+    this.y = y;
+    this.size = 100;
 
-    
+    // setting beginning state and cooking variables
+    this.state = "raw";
+    this.isDragging = false;
+    this.onGrill = false;
+  }
+
+  updatePatty() {
+    if (this.onGrill) {
+      cookingTime ++;   // fix these somenumbers
+      if (cookingTime < SOMENUMBERHERE) {
+        this.state = "raw"
+      }
+      else if (cookingtime >= SOMENUMBER && cookingTime <= SOMEOTHERNUMBER) {
+        this.state = "cooked"
+      }
+      else {
+        this.state = "burnt"
+      }
+    }
   }
 }
 
@@ -38,6 +63,6 @@ function draw() {
 
 
 
-// Reciept in corner - can be brought out/pushed in
+// Receipt in corner - can be brought out/pushed in
 
 // Introductory Dialogue, background and gather player name/ tutorial that can be SKIPPED
